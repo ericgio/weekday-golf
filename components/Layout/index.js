53b25f99e79c55950381441b80542a59a7dbefc4
Eric Giovanola <ericgio@gmail.com>
2020-02-07 00:52:58 -0800

    CSS Updates

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -44,6 +44,7 @@ const Layout = ({ children, rounds, stats, title }) => {
         />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="/base.css" />
       </Head>
       <Navbar
         className="app-nav justify-content-between"
