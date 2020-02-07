f3f32d47d1f438ee12d295c7643f3e298a2cfa90
Eric Giovanola <ericgio@gmail.com>
2020-02-06 22:04:58 -0800

    Add favicon

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,10 @@ const IndexPage = ({ rounds, stats }) => {
     <Fragment>
       <Head>
         <title>{title}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
       </Head>
       <div className="index-page">
