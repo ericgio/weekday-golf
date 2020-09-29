1bca486edacc38d388ab979036a4ae5d067707a3
Makinde Adeagbo <makinde@adeagbo.com>
2020-09-28 23:26:53 -0700

    Making tooltips work on mobile. (#9)
    CSS turtles all the way down.

--- pages/stats.js
+++ pages/stats.js
@@ -213,6 +213,9 @@ const StatsPage = ({ stats }) => {
                       })}
                       key={`avgHoleScore-${name}-${hole}`}
                       // eslint-disable-next-line max-len
+                      // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+                      tabIndex={0}
+                      // eslint-disable-next-line max-len
                       title={`Recent: ${recentAvgHoleScore}, All-time: ${avgHoleScore}`}>
                       {recentAvgHoleScore}
                       {trend > TRENDING_BUFFER && (
