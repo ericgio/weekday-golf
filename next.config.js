11f7ac4beb16a9e1ebbdc127dae68c8377fa7f56
Makinde Adeagbo <makinde@adeagbo.com>
2020-10-22 16:30:25 -0700

    Adding an `analyze` command
    So we can see the bundle. Nice and simple.
    
    Fixes #20

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer');
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = bundleAnalyzer({
   async redirects() {
     return [
       {
@@ -8,4 +14,4 @@ module.exports = {
       },
     ];
   },
-};
+});
